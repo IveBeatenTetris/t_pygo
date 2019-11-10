5464142da7d101ce83be6b24dda04ca9cab3e524
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2019-11-11 00:11:10 +0100

    added animations

--- entity.py
+++ entity.py
@@ -1,5 +1,6 @@
 from .utils import (
     PATH,
+    validateDict,
     loadAssets,
     draw,
     getFrames,
@@ -26,11 +27,15 @@ class Entity(pg.sprite.Sprite):
             animations or quickly chaning sprite-image.
         'image' based of a frame out of 'self.frames'.
         'rect' is a pyzero.zrect for smoother mooving-transitions.
+        'animationspeed' determines how fast an animation will run.
+        'animations' dict of animation objects.
         'anchors' is used for quick-pointing a part of the rect. for example:
             draw(object, self, self.anchors["midcenter"]).
         'collisionbox' the actual box for calculating collisions with.
-        'speed' is used to determine the moving speed of the entity. this is by
+        'speed' is used as the moving speed of the entity. this is by
             the way the amount of pixels that the entity moves per frame.
+        'facing' is used for determining the right picture for entity to
+            display.
         'dev_move' if 'true' this will render the entity bounding borders.
         """
         # looking for a json-file to use as the config
@@ -48,9 +53,33 @@ class Entity(pg.sprite.Sprite):
         self.frames = getFrames(self.rawimage, self.size)# list
         self.image = self.frames[0]# pygame.surface
         self.rect = ZRect(self.image.get_rect())# pgzero.zrect
+        self.animationspeed = self.config["animationspeed"]# int
+        self.animations = {# dict
+            "walkdown": Animation({
+                "frames": self.frames,
+                "sequence": [4, 5, 6, 7],
+                "duration": self.animationspeed
+                }),
+            "walkleft": Animation({
+                "frames": self.frames,
+                "sequence": [8, 9, 10, 11],
+                "duration": self.animationspeed
+                }),
+            "walkup": Animation({
+                "frames": self.frames,
+                "sequence": [12, 13, 14, 15],
+                "duration": self.animationspeed
+                }),
+            "walkright": Animation({
+                "frames": self.frames,
+                "sequence": [16, 17, 18, 19],
+                "duration": self.animationspeed
+                })
+            }
         self.anchors = getAnchors(self.rect.size)# dict
         self.collisionbox = pg.Rect(self.config["collisionbox"])# pygame.rect
         self.speed = self.config["speed"]# int
+        self.facing = "down"# str
         self.dev_mode = self.config["dev_mode"]# bool
         # keeping __init__ organazied
         self.__build()
@@ -97,17 +126,53 @@ class Entity(pg.sprite.Sprite):
                 if pos[1] < 0:
                     self.rect.top = block.bottom - rect.top
     def move(self, blocks=[]):
-        """move the entity to the given coordinates."""
+        """moving entity. also animate while still moving."""
         keys = pg.key.get_pressed()
-        # moving
+        moving = False
+        speed = self.speed
+        # shift key is pressed
+        if pg.key.get_mods() & pg.KMOD_SHIFT:
+            speed = int(self.speed * 1.5)
+        # moving and correct facing direction
         if keys[pg.K_a]:
-            self.__moveSingleAxis((-self.speed, 0), blocks)
+            self.__moveSingleAxis((-speed, 0), blocks)
+            self.facing = "left"
+            moving = True
         if keys[pg.K_d]:
-            self.__moveSingleAxis((self.speed, 0), blocks)
+            self.__moveSingleAxis((speed, 0), blocks)
+            self.facing = "right"
+            moving = True
         if keys[pg.K_w]:
-            self.__moveSingleAxis((0, -self.speed), blocks)
+            self.__moveSingleAxis((0, -speed), blocks)
+            self.facing = "up"
+            moving = True
         if keys[pg.K_s]:
-            self.__moveSingleAxis((0, self.speed), blocks)
+            self.__moveSingleAxis((0, speed), blocks)
+            self.facing = "down"
+            moving = True
+        # walking cycle animation
+        if moving:
+            if self.facing == "down":
+                name = "walkdown"
+            elif self.facing == "left":
+                name = "walkleft"
+            elif self.facing == "up":
+                name = "walkup"
+            elif self.facing == "right":
+                name = "walkright"
+
+            self.image = self.animations[name].image
+            self.animations[name].update()
+        # idle facing direction
+        else:
+            if self.facing == "down":
+                self.image = self.frames[0]
+            elif self.facing == "left":
+                self.image = self.frames[1]
+            elif self.facing == "up":
+                self.image = self.frames[2]
+            elif self.facing == "right":
+                self.image = self.frames[3]
     def position(self, pos=(0, 0)):
         """reposition of the entity sprite. updates the rect."""
         if type(pos) is pg.Rect:
@@ -119,3 +184,52 @@ class Player(Entity):
     def __init__(self, name):
         """inherit everything from its master class."""
         Entity.__init__(self, name)
+class Animation(pg.sprite.Sprite):
+    """
+    an animated sprite class. calling it would look like this:
+    anim = Animation({
+        "frames": entity.frames,
+        "sequence": [4, 5, 6, 7],
+        "duration": entity.animationspeed
+        })
+    """
+    default = {
+        "frames": [],
+        "sequence": [],
+        "duration": 100
+        }
+    def __init__(self, config={}):
+        """."""
+        self.config = validateDict(config, self.default)# dict
+        self.sequence = self.config["sequence"]# tuple / list
+        self.duration = self.config["duration"]# int
+        self.frames = self.config["frames"][# list
+            self.sequence[0]: self.sequence[-1] + 1
+        ]
+        pg.sprite.Sprite.__init__(self)# pygame.sprite
+        self.pointer = 0# int
+        self.image = self.frames[self.pointer]# pygame.surface
+        self.framecount = len(self.frames)# int
+        self.timer = self.duration + 1# int
+        self.timemod = int(self.timer / self.framecount)# int
+    def update(self):
+        """
+        updating the pointers position. the active frame is always drawn to
+        the animation image surface.
+        """
+        for i in range(self.framecount):
+            if self.timer == int((i) * self.timemod):
+                self.nextFrame()
+
+        if self.timer == 0:
+            self.timer = self.duration + 1
+        else:
+            self.timer = self.timer - 1
+    def nextFrame(self):
+        """set the pointer to the next frame."""
+        if self.pointer < self.framecount - 1:
+            self.pointer += 1
+        else:
+            self.pointer = 0
+
+        self.image = self.frames[self.pointer]
