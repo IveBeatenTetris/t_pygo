a87d7d18a99e461810d7a48807b2aef364924e0a
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2019-11-14 01:36:37 +0100

    full analog controll for xbox 360 game pad

--- controller.py
+++ controller.py
@@ -5,25 +5,42 @@ class Controller(object):
     def __init__(self):
         """initiating controller if there is one."""
         try:
-            self = pg.joystick.Joystick(0)
-            self.init()
+            self.joystick = pg.joystick.Joystick(0)
+            self.joystick.init()
         except pg.error:
-            self = None
+            self.joystick = None
     def buttons(self, events):
         """
         looks for controller events. i want to scrap the given 'events'
         property.
         """
         buttons = {
+            "select": False,
+            "start": False,
+            "lb": False,
+            "lt": False,
+            "rb": False,
+            "rt": False,
             "a": False,
             "x": False,
             "b": False,
-            "y": False
+            "y": False,
+            "up": False,
+            "down": False,
+            "left": False,
+            "right": False
         }
 
         for event in events:
             if event.type == pg.JOYBUTTONDOWN:
-                #print(event.dict, event.joy, event.button, 'pressed')
+                if event.button == 6:
+                    buttons["select"] = True
+                if event.button == 7:
+                    buttons["start"] = True
+                if event.button == 4:
+                    buttons["lb"] = True
+                if event.button == 5:
+                    buttons["rb"] = True
                 if event.button == 0:
                     buttons["a"] = True
                 if event.button == 2:
@@ -33,6 +50,67 @@ class Controller(object):
                 if event.button == 3:
                     buttons["y"] = True
             if event.type == pg.JOYAXISMOTION:
-                pass
+                if event.axis == 2:
+                    if round(event.value, 2) > 0.3:
+                        buttons["lt"] = True
+                    if round(event.value, 2) < -0.3:
+                        buttons["rt"] = True
+            if event.type == pg.JOYHATMOTION:
+                if event.value[1] == 1:
+                    buttons["up"] = True
+                elif event.value[1] == -1:
+                    buttons["down"] = True
+                if event.value[0] == 1:
+                    buttons["right"] = True
+                if event.value[0] == -1:
+                    buttons["left"] = True
 
         return buttons
+    def sticks(self, events):
+        """response for using the joysticks."""
+        left = {
+            "up": False,
+            "down": False,
+            "left": False,
+            "right": False,
+            "click": False
+        }
+        right = {
+            "up": False,
+            "down": False,
+            "left": False,
+            "right": False,
+            "click": False
+        }
+
+        for event in events:
+            if event.type == pg.JOYAXISMOTION:
+                # left stick
+                if event.axis == 0:
+                    if round(event.value, 2) < -0.3:
+                        left["left"] = True
+                    elif round(event.value, 2) > 0.3:
+                        left["right"] = True
+                elif event.axis == 1:
+                    if round(event.value, 2) < -0.3:
+                        left["up"] = True
+                    elif round(event.value, 2) > 0.3:
+                        left["down"] = True
+                # right stick
+                if event.axis == 4:
+                    if round(event.value, 2) < -0.3:
+                        right["left"] = True
+                    elif round(event.value, 2) > 0.3:
+                        right["right"] = True
+                elif event.axis == 3:
+                    if round(event.value, 2) < -0.3:
+                        right["up"] = True
+                    elif round(event.value, 2) > 0.3:
+                        right["down"] = True
+            elif event.type == pg.JOYBUTTONDOWN:
+                if event.button == 8:
+                    left["click"] = True
+                if event.button == 9:
+                    right["click"] = True
+
+        return [left, right]
