1e3e840c8d88da9693dd6ee9187d1d32e96550bf
IveBeatenTetris <ive_beaten_tetris@geekmail.de>
2019-11-22 10:04:14 +0100

    fetched text with gui.py

--- __init__.py
+++ __init__.py
@@ -2,13 +2,13 @@
 
 from .utils import *
 from .window import Window
-from .text import Text
 from .map import Tileset, Map
 from .entity import Player
 from .camera import Camera
 from .gui import (
     Button,
     Overlay,
+    Text,
     TextBox
 )
 from .controller import Controller
